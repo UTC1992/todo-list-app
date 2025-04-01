a1714bf6cd71ce7a0548730c2eaa874f75162476
Omar Guanoluisa <omar.guanoluisa25@gmail.com>
2025-04-01 17:15:25 -0500

    update

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, LoginFormComponent],
 })
 export class AuthModule {}
--- src/app/modules/auth/pages/login-home/login-home.component.ts
+++ src/app/modules/auth/pages/login-home/login-home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from '../../components/login-form/login-form.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-login-home',
-  imports: [LoginFormComponent],
+  imports: [LoginFormComponent, MatGridListModule],
   templateUrl: './login-home.component.html',
   styleUrl: './login-home.component.scss',
 })
