3c9bc4bc2dc468994825b8d39b93e57b774fddba
Omar Guanoluisa <omar.guanoluisa25@gmail.com>
2025-03-31 19:44:14 -0500

    add items to create form to login

--- src/app/modules/auth/components/login-form/login-form.component.ts
+++ src/app/modules/auth/components/login-form/login-form.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.scss'
+  styleUrl: './login-form.component.scss',
+  imports: [MatCardModule, MatFormFieldModule, MatButtonModule, MatInputModule],
 })
-export class LoginFormComponent {
-
-}
+export class LoginFormComponent {}
